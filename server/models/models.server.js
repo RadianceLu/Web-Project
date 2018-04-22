a89490221b90f7d320a7796407450d0ca60a9328
jielu <radiancelu@gmail.com>
2018-04-21 20:08:21 -0700

    try to deploy to heroku

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/cs5610project');
-// var db = mongoose.connect('mongodb://laurazha_cs5610project:cs5610@ds251889.mlab.com:51889/heroku_2j2bj958');
+// var db = mongoose.connect('mongodb://localhost:27017/cs5610project');
+var db = mongoose.connect('mongodb://jielu:jielumongodb2@ds153869.mlab.com:53869/heroku_2hrq6d47');
+
 
 module.exports = db;
